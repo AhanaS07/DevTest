5af49fdae1efb03be5f08f790f1b7afa4166f8e7
ibaad ahmed <ahmedibaad96@gmail.com>
2024-12-24 10:57:32 +0530

    Commit addsub and files

---
+++ addsub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("\nEnter 2 numbers - ");
+    scanf("%d %d",&a,&b);
+    printf("\nSum = %d",a+b);
+    printf("\nDifference = %d",a-b);
+    return 0;
+}
