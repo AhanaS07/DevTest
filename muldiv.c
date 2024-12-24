cd7cad3d3f5eac567999283422daf3ddc7c5fc86
AhanaS07 <ahanasrinathmurthy@gmail.com>
2024-12-24 11:17:06 +0530

    Branch new_feature Committed. Muldiv added

---
+++ muldiv.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("\nEnter 2 numbers - ");
+    scanf("%d %d",&a,&b);
+    printf("\nProduct = %d",a*b);
+    printf("\nQuotient = %d",a/b);
+    return 0;
+}
